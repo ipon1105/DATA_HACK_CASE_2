446c61b5f56b777ef301d266110daed82f080d96
ipon1105 <71529066+ipon1105@users.noreply.github.com>
2022-08-05 19:09:37 +0300

    Complete the #1 task
    1) The table for which synthetic data will be generated should be described in a separate python script. The description of the table structure should be a dataclass. Supported data types for columns: string, numbers (integers and fractions), fields in date and timestamp formats.

---
+++ DATA_HACK_CASE_2_PROJECT/Table.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+'''
+    This python script describes a table
+    for which synthetic data will be generated. 
+    The description of the table structure is a dataclass.
+'''
+
+@dataclass
+class Column:
+    "The class denotes a database column"
+
+    # The name field describes the column name
+    name: str
+
+    # The type field describes one of the possible column types
+    type: int
+
+@dataclass
+class Table:
+    "The class denotes a database table"
+
+    # The name field describes the table name
+    name: str
+
+    # The primary_index field describes the primary field index
+    primary_index: int
+
+    # The column_array field describes array of Column elements
+    column_array = list(Column)
