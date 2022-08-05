da5e2edfb7eec422f49e505db997ba60a2668c57
ipon1105 <71529066+ipon1105@users.noreply.github.com>
2022-08-05 20:05:26 +0300

    Create Config.py
    Add basement configuration data

---
+++ DATA_HACK_CASE_2_PROJECT/Config.py
@@ -0,0 +1,29 @@
+
+'''
+    This python script describes a Configuration information
+'''
+
+# The default types of data
+CONFIG_TYPE_0 = CONFIG_TYPE_INT         = int(0)  #int
+CONFIG_TYPE_1 = CONFIG_TYPE_STR         = int(1)  #string
+CONFIG_TYPE_2 = CONFIG_TYPE_FLOAT       = int(2)  #float
+CONFIG_TYPE_3 = CONFIG_TYPE_DATE        = int(3)  #date
+CONFIG_TYPE_4 = CONFIG_TYPE_TIMESTAMP   = int(4)  #timestamp
+
+# Count of rows to generator
+FIELD_COUNT = int(100)
+
+# Language to localization the massive of data
+LOCALIZATION = str("ru_RU")
+
+# Minimal year of age
+# MIN_YEAR_AGE = int(18)
+MIN_YEAR_AGE = str("-30y")
+
+# Maximum year of age
+# MAX_YEAR_AGE = int(26)
+MAX_YEAR_AGE = str("-20y")
+
+# Start years of live
+# START_YEAR = int(0)
+START_YEAR = str("-30y")
