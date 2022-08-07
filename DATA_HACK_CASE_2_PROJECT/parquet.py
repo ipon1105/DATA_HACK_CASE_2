7f9b95ce17ef460017e7344c8fdfffc39b4c2c34
lordcraft228 <72163037+lordcraft228@users.noreply.github.com>
2022-08-07 12:32:13 +0300

    join+parquet

--- DATA_HACK_CASE_2_PROJECT/parquet.py
+++ DATA_HACK_CASE_2_PROJECT/parquet.py
@@ -10,11 +10,4 @@ df = pd.DataFrame({'one': [-1, np.nan, 2.5],
                    index=list('abc'))
 
 
-table = pa.Table.from_pandas(df)
-pq.write_table(table, 'example.parquet')
-table2 = pq.read_table('example.parquet')
-
-table2.to_pandas()
-pq.read_table('example.parquet', columns=['one', 'three'])
-pq.read_pandas('example.parquet', columns=['two']).to_pandas()
 print(df)
